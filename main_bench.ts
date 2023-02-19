933b83e945844ac0ea990c50dc9260242efc215d
Robin DIDIER <dev@pulsarr.fr>
2023-02-19 08:32:44 +0100

    generate graph inside bench

--- main_bench.ts
+++ main_bench.ts
@@ -2,12 +2,14 @@ import {shortestPathSingleWay} from './main.ts';
 import {generateRandomGraph} from './random-graph.ts';
 import Node from './Node.ts'
 
-const nodes: Node[] = generateRandomGraph(1000000, 2)
+
 
 Deno.bench("shortest path single way", () => {
+    const nodes: Node[] = generateRandomGraph(Math.random() * 100000, Math.random() * 5)
     shortestPathSingleWay(nodes[0], nodes[nodes.length - 1])
 })
 
 Deno.bench("shortest path both way", () => {
+    const nodes: Node[] = generateRandomGraph(Math.random() * 100000, Math.random() * 5)
     shortestPathSingleWay(nodes[0], nodes[nodes.length - 1])
 })
